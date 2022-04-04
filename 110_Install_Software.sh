ec5e7006314786bb6af9f48cee086a023f4c1f92
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2022-04-04 15:39:23 +0100

    Update 110_Install_Software.sh

--- 110_Install_Software.sh
+++ 110_Install_Software.sh
@@ -2,5 +2,5 @@
 
 sudo apt install -y i3 dmenu fonts-font-awesome fonts-fork-awesome xfce4-terminal thunar gdebi polybar feh pulsemixer neofetch sxhkd curl dmenu rofi pcmanfm picom lxtask lxappearance lxpolkit aptitude moc lxterminal geany
 sudo apt install -y bspwm make gcc dunst libx11-dev libxft-dev libxinerama-dev fonts-noto-mono calcurse meson cmake libpulse-dev libcxxopts-dev fonts-jetbrains-mono i3lock-fancy htop gkrellm libnotify-bin herbstluftwm 
-sudo apt install -y deepin-icon-theme parcellite plank awesome
+sudo apt install -y deepin-icon-theme parcellite plank 
 #sudo apt install -y pamixer
