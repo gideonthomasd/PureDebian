5e13409aeb869d30be420971c3da7a1bad5e51be
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2022-04-05 22:31:02 +0100

    Create 190_Remmina.sh

---
+++ 190_Remmina.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo snap install remmina
+getsnap
+
+Click Add
+In protocol use RDP
+Fill in server as seniorwics.ddns.net:5630    (5631)
+username: deanp@wics
+password:
+and share folder in linux
