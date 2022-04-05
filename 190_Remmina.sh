a7e20e22744c03f3fad18c82421a326867f879ff
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2022-04-06 00:31:46 +0100

    Update 190_Remmina.sh

--- 190_Remmina.sh
+++ 190_Remmina.sh
@@ -3,9 +3,9 @@
 sudo snap install remmina
 getsnap
 
-Click Add
-In protocol use RDP
-Fill in server as seniorwics.ddns.net:5630    (5631)
-username: deanp@wics
-password:
-and share folder in linux
+#Click Add
+#In protocol use RDP
+#Fill in server as seniorwics.ddns.net:5630    (5631)
+#username: deanp@wics
+#password:
+#and share folder in linux
