3a22d48564fc431bc7e6aea87d10b81a74678b97
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2022-04-05 08:56:41 +0100

    Add files via upload

---
+++ 160-After-Reboot-Python.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo snap install pycharm-community --edge --classic
+#sudo snap install pycharm-community --classic
+sudo apt install -y python3-tk
+getsnap
---
+++ 170Printing.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo apt install -y printer-driver-all cups printer-driver-brlaser
+sudo apt install -y system-config-printer
